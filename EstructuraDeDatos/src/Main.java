82360a79d2bbcbbd37ddd835f18021af3ede2c3a
Jxel117 <stalintapia@unl.edu.ec>
2024-06-19 22:41:04 -0500

    CodigoComple2
    CodigoFinal

--- EstructuraDeDatos/src/Main.java
+++ EstructuraDeDatos/src/Main.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el número de ramas: ");
+        System.out.println("Arbol Avl presentado en Orden, Preorden y Postorden");
+        System.out.print("Ingrese el numero de ramas para el arbol Avl: ");
         int numeroDeRamas = scanner.nextInt();
 
         Arbol arbol = new ArbolAvl();
         for (int i = 0; i < numeroDeRamas; i++) {
             arbol.ingresarValor("Rama " + (i + 1));
         }
-
-        System.out.println("Valores en el árbol en orden:");
+        System.out.println("Orden:");
         arbol.enOrden();
-        System.out.println("\nValores en el árbol en preorden:");
+        System.out.println("\nPreorden:");
         arbol.preOrden();
-        System.out.println("\nValores en el árbol en postorden:");
+        System.out.println("\nPostorden:");
         arbol.postOrden();
 
         scanner.close();
